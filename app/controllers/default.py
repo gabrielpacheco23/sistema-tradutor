e03e712501c28f0b8a35b76a5f65db5942cebd6e
gabrielpacheco23 <gabrielpacheco23@live.com>
2018-05-12 20:57:31 -0300

    Traduções exibidas no terminal (funcionando) a partir das mensagens do chat

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,16 +1,25 @@
-from flask import render_template, request, jsonify
+from flask import render_template, request, json
 from app import app, db, socketio
 from flask_socketio import SocketIO, send
+from googletrans import Translator
+#import pydeepl
 
 #from app.models.tables import User
 #from app.models.forms import LoginForm
 
+
 @app.route("/index")
 @app.route("/")
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
+
+translator = Translator()
+
+lan = translator.detect('How are you my friend?')
 
 @socketio.on('message')
 def handle_messages(json):
     print('Receveid something' + str(json))
+    translation = translator.translate(str(json['msg']), src='pt')
+    print("Tradução:" + translation.text)
     socketio.emit('response', json)
